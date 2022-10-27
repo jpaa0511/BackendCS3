cb70c6636db5e03903b0aedb663448348af116f6
jpaa0511 <110578643+jpaa0511@users.noreply.github.com>
2022-10-27 01:33:43 -0500

    agrega plataforma con modelo d capas, index tmb

---
+++ build/model/plataforma.model.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Plataforma = void 0;
+class Plataforma {
+    constructor() {
+        this.Id = 0;
+        this.Imagen_URL = "";
+        this.Nombre = "";
+        this.Descripcion = "";
+        this.Precio = 0;
+    }
+}
+exports.Plataforma = Plataforma;
